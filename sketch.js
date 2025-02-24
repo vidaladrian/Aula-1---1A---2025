eb721fb072f269a7e22fd3520fae69ec6caf0370
adrian <vidal.adrian@escola.pr.gov.br>
2025-02-24 08:21:35 -0300

    24-02

---
+++ sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  //funcção draw significa desenhar
+  function draw() {
+    background(220,220,220);
+    circle(50,50,100);
+    circle(150,50,100);
+    circle(250,50,100);
+    circle(350,50,100);
+    circle(50,350,100);
+    circle(150,350,100);
+    circle(250,350,100);
+    circle(350,350,100);
+   
+  }
+  
+    
+    
+    
+
+  
